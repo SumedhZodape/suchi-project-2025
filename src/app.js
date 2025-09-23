a0b59ee6a20507f004af216058af9da3fb018625
Sumedh Zodape <sumedhzodape143@gmail.com>
2025-09-24 00:17:39 +0530

    cors changes

--- src/app.js
+++ src/app.js
@@ -7,9 +7,11 @@ const app = express();
 
 // Define your allowed origins (add your actual domains here)
 const allowedOrigins = [
+  'http://localhost:5173',
   'http://localhost:8080',
   'http://localhost:3000', 
   'http://127.0.0.1:8080',
+  'http://127.0.0.1:5173',
   'https://yourdomain.com', // Replace with your actual domain
   'https://another-frontend-domain.com'
 ];
